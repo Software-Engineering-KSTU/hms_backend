f2ffb2fce7682e817baee70dfa662cb262dad73c
beknazaarr <beknazaarr@gmail.com>
2025-10-02 02:07:44 +0600

    Начальный проект Django с настройкой Postgres

--- hms/settings.py
+++ hms/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "hms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hms_Data_Base",     
+        "USER": "root",              
+        "PASSWORD": "password",     
+        "HOST": "localhost",        
+        "PORT": "5432",              
     }
 }
 
