9a461e10606877fd04c483b69a1f968e1ae6a168
Genma-shi <baisalovdastan150505@gmail.com>
2025-10-23 13:52:19 +0600

    fix patient model

--- users/models.py
+++ users/models.py
@@ -78,16 +78,3 @@ class User(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-class PatientProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient_profile')
-    date_of_birth = models.DateField()
-    address = models.CharField(max_length=255)
-    emergency_contact_name = models.CharField(max_length=100)
-    emergency_contact_phone = models.CharField(max_length=20)
-    medical_history = models.TextField()
-
-    class Meta:
-        db_table = 'patients'
-
-    def __str__(self):
-        return self.user.__str__()
